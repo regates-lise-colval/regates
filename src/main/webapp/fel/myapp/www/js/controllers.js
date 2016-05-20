0791dd8a6a1287371d3878989950114f586398d4
Vincent Laude <Vincent Laude>
2016-05-20 09:33:01 -0400

    start new app

---
+++ src/main/webapp/fel/myapp/www/js/controllers.js
@@ -0,0 +1,77 @@
+angular.module('starter.controllers', [])
+
+.controller('EventsCtrl', function($scope, $http, Events) {
+
+  /*$http({
+  method: 'GET',
+  url: 'http://localhost:8080/regates/webresources/events'
+  }).then(function successCallback(response) {
+
+    Events.events = response.data;
+    $scope.events = Events.events;
+
+  }, function errorCallback(response) {
+  });*/
+  //console.log(Events.events);
+  //$scope.events = Events.all();
+  console.log(events);
+  $scope.events = Events.all();
+  $scope.remove = function(event) {
+    Events.remove(event);
+  };
+})
+
+.controller('EventInfoCtrl', function($scope, $stateParams, $ionicNavBarDelegate, Events, Ships) {
+  /*
+  $http({
+  method: 'GET',
+  url: 'http://localhost:8080/regates/webresources/events/' + $stateParams.eventId
+  }).then(function successCallback(response) {
+    $scope.event = response.data;
+    console.log(response.data[0]);
+  }, function errorCallback(response) {
+  });
+  */
+  $scope.event = Events.get($stateParams.eventId);
+  $scope.ships = Ships.all();
+  $scope.remove = function(ships) {
+    Ships.remove(ships);
+  };
+})
+
+.controller('EventScoreCtrl', function($scope, $stateParams, Events, Ships) {
+
+  $scope.event = Events.get($stateParams.eventId);
+  $scope.ships = Ships.all();
+  $scope.remove = function(ships) {
+    Ships.remove(ships);
+  };
+})
+
+.controller('ShipsCtrl', function($scope, Ships) {
+  $scope.ships = Ships.all();
+  $scope.remove = function(ships) {
+    Ships.remove(ships);
+  };
+})
+
+.controller('ContestsCtrl', function($scope, Contests) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.contests = Contests.all();
+  $scope.remove = function(contests) {
+    Contests.remove(contests);
+  };
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
---
+++ src/main/webapp/fel/myapp/www/js/eventsServices.js
@@ -0,0 +1,96 @@
+angular.module('events.services', [])
+
+.factory('Events', function($http) {
+
+    var events;
+
+    $http({
+    method: 'GET',
+    url: 'http://localhost:8080/regates/webresources/events'
+    }).then(function successCallback(response) {
+        events = response.data;
+    }, function errorCallback(response) {
+
+    });
+    
+    /*
+    var events = [{
+      id: 1,
+      city: 'Détroit',
+      category: 'Hydro 350',
+      date: '2016/06/20',
+      img: 'detroit.jpg'
+    }, {
+      id: 2,
+      city: 'Stuart',
+      category: 'Hydro 350',
+      date: '2016/06/20',
+      img: 'stuart.jpg'
+    }, {
+      id: 3,
+      city: 'Valleyfield',
+      category: 'Hydro 350',
+      date: '2016/06/20',
+      img: 'Valleyfield.jpg'
+    }, {
+      id: 4,
+      city: 'buffalo',
+      category: 'Hydro 350',
+      date: '2016/06/20',
+      img: 'buffalo.jpg'
+    }, {
+      id: 5,
+      city: 'St Felicien',
+      category: 'Hydro 350',
+      date: '2016/06/20',
+      img: 'st_felicien.jpg'
+    }, {
+      id: 6,
+      city: 'Détroit',
+      category: 'Hydro 350',
+      date: '2016/06/20',
+      img: 'detroit.jpg'
+    }, {
+      id: 7,
+      city: 'Valleyfield',
+      category: 'Hydro 350',
+      date: '2016/06/20',
+      img: 'Valleyfield.jpg'
+    }, {
+      id: 8,
+      city: 'Buffalo',
+      category: 'Hydro 350',
+      date: '2016/06/20',
+      img: 'buffalo.jpg'
+    }, {
+      id: 9,
+      city: 'Détroit',
+      category: 'Hydro 350',
+      date: '2016/06/20',
+      img: 'detroit.jpg'
+    }, {
+      id: 10,
+      city: 'Stuart',
+      category: 'Hydro 350',
+      date: '2016/06/20',
+      img: 'stuart.jpg'
+    }];
+    */
+
+    return {
+      all: function() {
+        return events;
+      },
+      remove: function(event) {
+        events.splice(events.indexOf(event), 1);
+      },
+      get: function(eventId) {
+        for (var i = 0; i < events.length; i++) {
+          if (events[i].id === parseInt(eventId)) {
+            return events[i];
+          }
+        }
+        return null;
+      }
+    };
+});
